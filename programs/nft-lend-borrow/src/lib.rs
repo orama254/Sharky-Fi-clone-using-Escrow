ad1d0ec280d6d2b5bdbb36be2ac0a86112467948
orama254 <orama209@gmail.com>
2024-06-17 04:25:37 +0300

    feat: nft defi init

--- programs/nft-lend-borrow/src/lib.rs
+++ programs/nft-lend-borrow/src/lib.rs
@@ -6,7 +6,7 @@ pub use errors::ErrorCodes;
 pub use instructions::*;
 pub use states::*;
 
-declare_id!("<PLACE YOUR ADDRESS HERE>");
+declare_id!("8zfuZ2LJFWkHVQcXN8UPL5QzM7PGB9pWoVaxiEaquR68");
 
 #[program]
 pub mod nft_lend_borrow {
